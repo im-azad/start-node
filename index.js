ab756bf92fc2a2c5d36dadf861f35cd3c0b81b33
Abul Kalam Azad <me.azad99@gmail.com>
2022-07-23 12:44:09 +0600

    api response by get

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//basic
 const express = require('express');
 const app = express();
 const port = 5000; //process.env.PORT || 3000;
 
+// data of users
 const users = [
 	{ id: 1, name: 'Azad', email: 'azad@gmail.com', phone: '01711000001' },
 	{ id: 2, name: 'Kalam', email: 'kalam@gmail.com', phone: '01711000002' },
@@ -14,14 +16,23 @@ const users = [
 	{ id: 9, name: 'Rahim', email: 'rahim@gmail.com', phone: '01711000009' },
 	{ id: 10, name: 'Rabbi', email: 'rabbi@gmail.com', phone: '01711000010' },
 ];
+//data of fruits
+const fruits = {
+	mangoes: ['Amrupli', 'Langra', 'Harivanga', 'Khisapath'],
+	bananas: ['Suprikola', 'Sagorkola'],
+	guavas: ['Kazi', 'Thi'],
+};
 
+// basic
 app.get('/', (req, res) => {
 	res.send('Hello world, Node js learning');
 });
 
+//users data response
 app.get('/users', (req, res) => {
 	res.send(users);
 });
+// users data dynamic
 app.get('/users/:id', (req, res) => {
 	// console.log(req.params.id);
 	const index = req.params.id;
@@ -29,6 +40,23 @@ app.get('/users/:id', (req, res) => {
 	// console.log(user);
 	res.send(user);
 });
+
+// fruits data response
+app.get('/fruits', (req, res) => {
+	res.send(fruits);
+});
+
+app.get('/fruits/mangoes', (req, res) => {
+	res.send(fruits.mangoes);
+});
+app.get('/fruits/bananas', (req, res) => {
+	res.send(fruits.bananas);
+});
+app.get('/fruits/guavas', (req, res) => {
+	res.send(fruits.guavas);
+});
+
+// basic
 app.listen(port, () => {
 	console.log(`listening on port ${port}`);
 });
